653cbd5756b011bb0d30076d3b2059dd35eb668b
Akhmad Qasim <akhmadqasim@gmail.com>
2024-02-29 20:10:42 +0800

    Initial Commit

---
+++ main.dart
@@ -0,0 +1,19 @@
+// ignore: depend_on_referenced_packages,
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/column_widget.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(title: 'Aplikasi Flutter Pertama',
+    home: ColumnWidget()
+    );
+  }
+}
